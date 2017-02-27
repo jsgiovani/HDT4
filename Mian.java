f222e46565e0c0ac8e516509dac61ab206b7d378
jsgiovani <josgiovani@hotmail.com>
2017-02-26 18:52:48 -0600

    Update Mian.java

--- Mian.java
+++ Mian.java
@@ -1,20 +1,18 @@
-import java.util.*;
 public class Mian {
 	
 
 	public static void main(String[] args) {
 		PilaArrayList mystack = new PilaArrayList();
 		
-		mystack.push(45);
-		mystack.push("hola");
-		mystack.push(69);
-		mystack.push(23);
-		mystack.push("Hola soy giovani");
-		mystack.push(32.4869);
-		
-		mystack.zice();
-		mystack.firstAbove();
-		mystack.peck();
+		mystack.empujar(45);
+		mystack.empujar("hola");
+		mystack.empujar(69);
+		mystack.empujar(23);
+		mystack.empujar("Hola soy giovani");
+		mystack.empujar(32.4869);
+		mystack.tamanoPila();
+		mystack.cima();
+		mystack.ultimo();
 		
 	}
 
