df46ebc030d8fcc6b26d622490e1f611ce769f1c
jsgiovani <josgiovani@hotmail.com>
2017-02-26 18:53:07 -0600

    Update PilaArrayList.java

--- PilaArrayList.java
+++ PilaArrayList.java
@@ -6,25 +6,25 @@ public class PilaArrayList <T>{
 		ArrayList <T> miArrayList = new ArrayList<T>();
 		
 		//Insertar valor en la pila
-		public void push(T dato){
+		public void empujar(T dato){
 			miArrayList.add(dato);
 		}
 		//Devuelbe el ultimo dato que esta en la pila
-		public void peck(){
+		public void cima(){
 			T ultimo = miArrayList.get(miArrayList.size()-1);
 			System.out.println("El primer valor en la pila es: " + ultimo);
 		}
 		
 		//Elimina el ultimo datoq que se inserto en la pila
-		public void pop(){
+		public void sacar(){
 			miArrayList.remove(miArrayList.size()-1);
 		}
-		//Devuelbe el tamaño de la lista
-		public void zice(){
-			System.out.println("El tamaño de la pila es: " +  miArrayList.size());
+		//Devuelbe el tamaÃ±o de la lista
+		public void tamanoPila(){
+			System.out.println("El tamaÃ±o de la pila es: " +  miArrayList.size());
 		}
 		//Devuelbe el primero en la lista
-		public void firstAbove(){
+		public void ultimo(){
 			T primero = miArrayList.get(0);
 			System.out.println("El ultimo valor en la pila es: " + primero);
 			
