f0aa51f07569454450a59a8b435d072cb1d88622
jsgiovani <josgiovani@hotmail.com>
2017-02-26 18:21:54 -0600

    Add files via upload

---
+++ Stackk.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+public class Stackk <T>{
+	
+		T dato;
+		
+		ArrayList <T> miArrayList = new ArrayList<T>();
+		
+		//Insertar valor en la pila
+		public void push(T dato){
+			miArrayList.add(dato);
+		}
+		//Devuelbe el ultimo dato que esta en la pila
+		public void peck(){
+			T ultimo = miArrayList.get(miArrayList.size()-10);
+			System.out.println("El ultimo valor en la pila es: " + ultimo);
+		}
+		
+		//Elimina el ultimo datoq que se inserto en la pila
+		public void pop(){
+			miArrayList.remove(miArrayList.size());
+		}
+		//Devuelbe el tamaño de la lista
+		public void zice(){
+			System.out.println("El tamaño de la pila es: " +  miArrayList.size());
+		}
+		//Devuelbe el primero en la lista
+		public void firstAbove(){
+			T primero = miArrayList.get(0);
+			System.out.println("El ultimo valor en la pila es: " + primero);
+			
+		}
+		
+	
+	}
+
+
